f3d78fb25f25b810f52e2c955e971a5ca7fc5007
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2025-04-20 17:06:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ keyboard.kbEvent(KeyValue.keyany, function () {
     keyboard.setIndexColor(Guess, 0x00ff00)
     basic.pause(100)
     basic.showNumber(Guess)
-    keyboard.ledBlank()
     if (Guess == Number2) {
         Correct()
     } else if (Guess < Number2) {
@@ -16,38 +15,58 @@ function TooLow () {
     lcdDisplay.lcdClearAll()
     keyboard.setIndexColor(Guess, 0xff0000)
     keyboard.vibrationMotor(OnOff.ON)
-    basic.pause(500)
+    basic.pause(200)
     keyboard.vibrationMotor(OnOff.OFF)
     lcdDisplay.lcdDisplayText("Too Low!!", 1, 120, 120, lcdDisplay.FontSize.Large, 0xFF0000)
+    basic.pause(100)
     keyboard.ledBlank()
+    basic.clearScreen()
+    lcdDisplay.lcdClearAll()
 }
 input.onButtonPressed(Button.A, function () {
+    lcdDisplay.lcdSetBgcolor(0xffffff)
     basic.clearScreen()
     keyboard.ledBlank()
     lcdDisplay.lcdClearAll()
     lcdDisplay.lcdDisplayText("Enter your guess on Keypad", 1, 10, 10, lcdDisplay.FontSize.Large, 0x0000ff)
     Number2 = randint(0, 9)
 })
+function Reward () {
+    for (let index = 0; index < 16; index++) {
+        lcdDisplay.lcdSetBgcolor(lcdDisplay.lcdGetRgbColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        basic.showIcon(IconNames.Happy)
+        basic.pause(100)
+        basic.clearScreen()
+    }
+}
 function TooHigh () {
     lcdDisplay.lcdClearAll()
     keyboard.setIndexColor(Guess, 0xff0000)
     keyboard.vibrationMotor(OnOff.ON)
-    basic.pause(500)
+    basic.pause(200)
     keyboard.vibrationMotor(OnOff.OFF)
-    keyboard.ledBlank()
     lcdDisplay.lcdDisplayText("Too High!!", 1, 120, 120, lcdDisplay.FontSize.Large, 0xFF0000)
+    basic.pause(100)
     basic.clearScreen()
+    keyboard.ledBlank()
     lcdDisplay.lcdClearAll()
 }
 function Correct () {
     lcdDisplay.lcdDisplayText("YOU GOT IT!!!", 1, 120, 120, lcdDisplay.FontSize.Large, 0xFF0000)
-    basic.pause(1000)
-    keyboard.ledRainbow(
-    0,
-    15,
-    1,
-    360
-    )
+    for (let index = 0; index < 16; index++) {
+        basic.pause(100)
+        keyboard.ledRainbow(
+        0,
+        15,
+        1,
+        360
+        )
+        basic.pause(100)
+        keyboard.ledBlank()
+    }
+    keyboard.ledBlank()
+    basic.clearScreen()
+    Reward()
 }
 let Number2 = 0
 let Guess = 0

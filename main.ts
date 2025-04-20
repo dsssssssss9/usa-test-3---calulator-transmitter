1b02801b6b01db6166542381e2120fc0f50c73e4
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2025-04-20 14:51:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,36 @@
 keyboard.kbEvent(KeyValue.keyany, function () {
-    Key = keyboard.keyBasic()
-    keyboard.setIndexColor(Key, 0x00ff00)
-    basic.showNumber(Key)
+    Guess = keyboard.keyBasic()
+    keyboard.setIndexColor(Guess, 0x00ff00)
+    basic.showNumber(Guess)
     basic.pause(100)
     keyboard.ledBlank()
+    if (Guess == Number2) {
+        Correct()
+    } else if (Guess < Number2) {
+        TooLow()
+    } else if (Guess > Number2) {
+        TooHigh()
+    }
 })
+function TooLow () {
+    keyboard.vibrationMotor(OnOff.ON)
+    basic.pause(1000)
+}
 input.onButtonPressed(Button.A, function () {
     lcdDisplay.lcdClearAll()
-    lcdDisplay.lcdDisplayText("Enter your guess on Keypad", 1, 20, 10, lcdDisplay.FontSize.Large, 0x0000ff)
+    lcdDisplay.lcdDisplayText("Enter your guess on Keypad", 1, 10, 10, lcdDisplay.FontSize.Large, 0x0000ff)
+    Number2 = randint(0, 9)
 })
-let Key = 0
+function TooHigh () {
+    keyboard.vibrationMotor(OnOff.ON)
+    basic.pause(1000)
+}
+function Correct () {
+	
+}
+let Number2 = 0
+let Guess = 0
+basic.showIcon(IconNames.Yes)
 lcdDisplay.lcdInitIIC()
 lcdDisplay.lcdClearAll()
 lcdDisplay.lcdSetBgcolor(0xffffff)

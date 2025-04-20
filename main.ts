182c878c97839bb51ccc90aad1ba41633b44d6b1
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2025-04-20 14:13:57 -0500

    Update pxt.json, main.blocks, main.ts, _locales/en-GB/README.md

--- main.ts
+++ main.ts
@@ -1,14 +1,11 @@
+let Key = 0
 keyboard.kbEvent(KeyValue.keyany, function () {
     Key = keyboard.keyBasic()
     keyboard.setIndexColor(Key, 0x00ff00)
     basic.showNumber(Key)
     basic.pause(100)
-    radio.sendNumber(Key)
     keyboard.ledBlank()
 })
-let Key = 0
-radio.setGroup(42)
-basic.showIcon(IconNames.Meh)
 basic.forever(function () {
 	
 })
